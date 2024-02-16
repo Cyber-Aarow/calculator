3df037772f51b204907137048e9b57f74c18802a
Cyber-Aarow <136511597+Cyber-Aarow@users.noreply.github.com>
2024-02-15 22:01:55 -0800

    Create JS file and copy other calculator functions from previous exercise.

---
+++ calculator.js
@@ -0,0 +1,29 @@
+const add = function(x, y) {
+	return x + y;
+};
+
+const subtract = function(x, y) {
+	return x - y;
+};
+
+const sum = function(array) {
+  final_sum = 0;
+	array.forEach((value) => final_sum = add(final_sum, value));
+  return final_sum;
+};
+
+const multiply = function(array) {
+  return array.reduce((product, current) => product * current)
+};
+
+const power = function(x, y) {
+	return x ** y;
+};
+
+const factorial = function(x) {
+  let final_product = 1;
+  for(let i = 2; i <= x; i++){
+    final_product *= i;
+  }
+  return final_product;
+};
